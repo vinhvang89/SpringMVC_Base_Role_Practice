77f2aae9f3fe12a4d5322f965e51dd214af993fd
vinhvang89 <61720675+vinhvang89@users.noreply.github.com>
2020-07-06 21:38:52 +0700

    new

---
+++ src/main/java/com/configure/AppInit.java
@@ -0,0 +1,20 @@
+package com.configure;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfig.class,SecConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/java/com/configure/SecInit.java
@@ -0,0 +1,6 @@
+package com.configure;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecInit extends AbstractSecurityWebApplicationInitializer {
+}
